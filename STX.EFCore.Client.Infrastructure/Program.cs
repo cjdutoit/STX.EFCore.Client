cb9f6ffcf37dfe2ebcac15604c3c0265a301246c
Christo du Toit <christo@dutoit.co.uk>
2024-09-19 19:17:01 +0100

    INFRA: Initialise projects

---
+++ STX.EFCore.Client.Infrastructure/Program.cs
@@ -0,0 +1,20 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using STX.EFCore.Client.Infrastructure.Services;
+
+namespace STX.EFCore.Client.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+
+            scriptGenerationService.GenerateBuildScript(
+                branchName: "main",
+                projectName: "GitFyle.Core.Api");
+        }
+    }
+}
