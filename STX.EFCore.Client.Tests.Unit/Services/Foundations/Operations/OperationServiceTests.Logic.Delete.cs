53fcac3eb78e548765b4f478fc8e905c5741e693
Christo du Toit <christo@dutoit.co.uk>
2024-09-22 23:16:41 +0100

    DeleteAsyncShouldMarkEntityAsDeletedSaveChangesAndDetach -> FAIL

---
+++ STX.EFCore.Client.Tests.Unit/Services/Foundations/Operations/OperationServiceTests.Logic.Delete.cs
@@ -0,0 +1,60 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Microsoft.EntityFrameworkCore;
+using STX.EFCore.Client.Services.Foundations;
+using STX.EFCore.Client.Tests.Unit.Brokers.Storages;
+using STX.EFCore.Client.Tests.Unit.Models.Foundations.Users;
+
+namespace STX.EFCore.Client.Tests.Unit.Services.Foundations.Operations
+{
+    public partial class OperationServiceTests
+    {
+        [Fact]
+        public async Task DeleteAsyncShouldMarkEntityAsDeletedSaveChangesAndDetach()
+        {
+            // Given
+            User randomUser = CreateRandomUser();
+            User inputUser = randomUser;
+            User deletedUser = inputUser;
+            User expectedUser = inputUser.DeepClone();
+            EntityState? stateBeforeSave = null;
+            EntityState? stateAfterSave = null;
+
+            var options = new DbContextOptionsBuilder<TestDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb").Options;
+
+            TestDbContext dbContext = new TestDbContext(options);
+            await dbContext.Users.AddAsync(inputUser);
+            await dbContext.SaveChangesAsync();
+            OperationService operationService = new OperationService(dbContext);
+
+            dbContext.SavingChanges += (sender, e) =>
+            {
+                var entry = dbContext.Entry(inputUser);
+                stateBeforeSave = entry.State;
+            };
+
+            dbContext.SavedChanges += (sender, e) =>
+            {
+                var entry = dbContext.Entry(inputUser);
+                stateAfterSave = entry.State;
+            };
+
+            // When
+            User actualUser = await operationService.DeleteAsync(inputUser);
+
+            // Then
+            stateBeforeSave.Should().Be(EntityState.Deleted);
+            stateAfterSave.Should().Be(EntityState.Detached);
+            var userInDatabase = await dbContext.Users.FindAsync(deletedUser.Id);
+            userInDatabase.Should().BeNull();
+            actualUser.Should().BeEquivalentTo(expectedUser);
+            await dbContext.Database.EnsureDeletedAsync();
+        }
+    }
+}
