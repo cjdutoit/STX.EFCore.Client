d31e89cf772b2af9eec22d6ea49101ae35554588
Christo du Toit <christo@dutoit.co.uk>
2024-10-01 23:47:20 +0100

    EXPOSERS: EFCore Client

---
+++ STX.EFCore.Client/Clients/EFCoreClient.cs
@@ -0,0 +1,43 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using STX.EFCore.Client.Services.Foundations;
+
+namespace STX.EFCore.Client.Clients
+{
+    public class EFCoreClient : IEFCoreClient
+    {
+        private readonly IOperationService operationService;
+
+        public EFCoreClient(DbContext dbContext)
+        {
+            this.operationService = new OperationService(dbContext);
+        }
+
+        public async ValueTask<T> InsertAsync<T>(T @object) where T : class =>
+            await this.operationService.InsertAsync(@object);
+
+        public async ValueTask BulkInsertAsync<T>(IEnumerable<T> objects) where T : class =>
+            await this.operationService.BulkInsertAsync(objects);
+
+        public async ValueTask<IQueryable<T>> SelectAllAsync<T>() where T : class =>
+            await this.operationService.SelectAllAsync<T>();
+
+        public async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class =>
+            await this.operationService.SelectAsync<T>(objectIds);
+
+        public async ValueTask<T> UpdateAsync<T>(T @object) where T : class =>
+            await this.operationService.UpdateAsync(@object);
+
+        public async ValueTask BulkUpdateAsync<T>(IEnumerable<T> objects) where T : class =>
+            await this.operationService.BulkUpdateAsync(objects);
+
+        public ValueTask<T> DeleteAsync<T>(T @object) where T : class =>
+            this.operationService.DeleteAsync(@object);
+    }
+}
---
+++ STX.EFCore.Client/Clients/IEFCoreClient.cs
@@ -0,0 +1,21 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STX.EFCore.Client.Clients
+{
+    public interface IEFCoreClient
+    {
+        ValueTask<T> InsertAsync<T>(T @object) where T : class;
+        ValueTask BulkInsertAsync<T>(IEnumerable<T> objects) where T : class;
+        ValueTask<IQueryable<T>> SelectAllAsync<T>() where T : class;
+        ValueTask<T> SelectAsync<T>(params object[] @objectIds) where T : class;
+        ValueTask<T> UpdateAsync<T>(T @object) where T : class;
+        ValueTask BulkUpdateAsync<T>(IEnumerable<T> objects) where T : class;
+        ValueTask<T> DeleteAsync<T>(T @object) where T : class;
+    }
+}
