f2063e3918fd9454aa869c6fbdbbba99e34271b0
Christo du Toit <christo@dutoit.co.uk>
2024-10-08 09:07:17 +0100

    BulkUpdateAsyncShouldRollbackRecordsOnErrorWithTransaction -> PASS

---
+++ STX.EFCore.Client.Tests.Unit/Services/Foundations/Operations/OperationServiceTests.Exceptions.BulkUpdate.cs
@@ -0,0 +1,66 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using STX.EFCore.Client.Tests.Unit.Models.Foundations.Users;
+
+namespace STX.EFCore.Client.Tests.Unit.Services.Foundations.Operations
+{
+    public partial class OperationServiceTests
+    {
+        [Fact]
+        public async Task BulkUpdateAsyncShouldRollbackRecordsOnErrorWithTransaction()
+        {
+            // Given
+            bool useTransaction = true;
+            IEnumerable<User> randomUsers = CreateRandomUsers();
+            IEnumerable<User> inputUsers = randomUsers;
+            Exception someException = new Exception(message: GetRandomString());
+            Exception expectedException = someException.DeepClone();
+
+            storageBrokerMock.Setup(broker =>
+                broker.BeginTransactionAsync())
+                    .ReturnsAsync(dbContextTransactionMock.Object);
+
+            storageBrokerMock.Setup(broker =>
+                broker.BulkUpdateAsync(It.IsAny<IEnumerable<User>>()))
+                    .ThrowsAsync(someException);
+
+            // When
+            ValueTask updateUserTask = operationService.BulkUpdateAsync(inputUsers, useTransaction);
+            Exception actualException = await Assert.ThrowsAsync<Exception>(updateUserTask.AsTask);
+
+            // Then
+            actualException.Message.Should().BeEquivalentTo(expectedException.Message);
+
+            storageBrokerMock.Verify(broker =>
+                broker.BeginTransactionAsync(),
+                    Times.Once);
+
+            storageBrokerMock.Verify(broker =>
+                broker.BulkUpdateAsync(inputUsers),
+                    Times.Once);
+
+            dbContextTransactionMock.Verify(transaction =>
+                transaction.RollbackAsync(default),
+                    Times.Once);
+
+            dbContextTransactionMock.Verify(transaction =>
+                transaction.Dispose(),
+                    Times.Once);
+
+            dbContextTransactionMock.Verify(transaction =>
+                transaction.CommitAsync(default),
+                    Times.Never);
+
+            storageBrokerMock.VerifyNoOtherCalls();
+            dbContextTransactionMock.VerifyNoOtherCalls();
+        }
+    }
+}
