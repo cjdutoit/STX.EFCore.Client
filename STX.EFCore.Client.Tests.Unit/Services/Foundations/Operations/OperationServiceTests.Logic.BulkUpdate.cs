2a4bd2a37e7793416a2306b8820aea8b2e34968f
Christo du Toit <christo@dutoit.co.uk>
2024-10-08 08:44:02 +0100

    BulkUpdateAsyncShouldUpdateAllTheRecordsWithTransaction -> PASS

--- STX.EFCore.Client.Tests.Unit/Services/Foundations/Operations/OperationServiceTests.Logic.BulkUpdate.cs
+++ STX.EFCore.Client.Tests.Unit/Services/Foundations/Operations/OperationServiceTests.Logic.BulkUpdate.cs
@@ -30,5 +30,41 @@ namespace STX.EFCore.Client.Tests.Unit.Services.Foundations.Operations
 
             storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task BulkUpdateAsyncShouldUpdateAllTheRecordsWithTransaction()
+        {
+            // Given
+            bool useTransaction = true;
+            IEnumerable<User> randomUsers = CreateRandomUsers();
+            IEnumerable<User> updatedUsers = randomUsers.DeepClone();
+
+            storageBrokerMock.Setup(broker =>
+                broker.BeginTransactionAsync())
+                    .ReturnsAsync(dbContextTransactionMock.Object);
+
+            // When
+            await operationService.BulkUpdateAsync(updatedUsers, useTransaction);
+
+            // Then
+            storageBrokerMock.Verify(broker =>
+                broker.BeginTransactionAsync(),
+                    Times.Once);
+
+            storageBrokerMock.Verify(broker =>
+                broker.BulkUpdateAsync(updatedUsers),
+                    Times.Once);
+
+            dbContextTransactionMock.Verify(transaction =>
+                transaction.CommitAsync(default),
+                    Times.Once);
+
+            dbContextTransactionMock.Verify(transaction =>
+                transaction.Dispose(),
+                    Times.Once);
+
+            storageBrokerMock.VerifyNoOtherCalls();
+            dbContextTransactionMock.VerifyNoOtherCalls();
+        }
     }
 }
